f6622b61e98fee7e506c31ad101f78b2e6bb0637
Tshiamo77 <138474997+Tshiamo77@users.noreply.github.com>
2026-03-27 02:44:18 +0200

    Board
    Tried running code. Will remove the launch thing.

--- morabaraba/main.cpp
+++ morabaraba/main.cpp
@@ -5,13 +5,9 @@
 
 using namespace std;
 
-const int EMPTY = 0; //f
+const int EMPTY = 0; 
 const int ALG_1 =1;
 const int ALG_2= 2;
-
-
-   
-
 
 class Intersection{
 public:
@@ -22,86 +18,82 @@ public:
     Intersection(): status(EMPTY) {}
     Intersection(string lbl) :
     label(lbl), status (EMPTY) {}
-
-
 };
-
 
 class Square{
     public:
         string name;
-        vector<Intersections> Intersections;
+        vector<Intersection> intersections;
 
     Square (string n) : name(n) {
         for (int j = 0; j<8; ++j){
-            intersections.push_back(Intersection(name + ",i" + to_string(j)));
+            intersections.push_back(Intersection(name + "i" + to_string(j)));
         }
     }
 
 };
-
 
 class Board{
 public:
-
-    ifstream input("input.txt");
-    ofstream output("output.txt");
-
     vector<Square> squares;
     int TotalNumPieces;
 
-    Board(string input, string output){
+    Board()
+    {
         squares.push_back(Square("S0"));
         squares.push_back(Square("S1"));
-        square.push_back(Square("S2"));
-        
-        if (input.is_open()){
-            input>> TotalNumPieces;
-        }
-        else{
-            TotalNumPieces=4;
-        }
-        }
-        }
-
+        squares.push_back(Square("S2"));
     }
-
-
-
-
-};
-
-
+        void placePiece(string pos, int player){
+            int sIdx= pos[1]- '0';
+            int iIdx= pos[3]- '0';
+            if (sIdx>=0 && sIdx< 3 && iIdx>=0 && iIdx<8){
+                squares[sIdx].intersections[iIdx].status= player;
+            }
+        }
+    };
+       
 int main()
-{
-    Game g;
+{ 
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+
+    if(!input.is_open()){
+        return 1;
+    }
     int cow;
-    int score;
-
-
-    g.input >> cow;
-
+    while(input>> cow){
+        Board b;
+        b.TotalNumPieces= cow;
+        output<<cow<<endl;
+    
+    
     switch(cow)
     {
 
-    case 4:
-        //something
-        break;
+    case 4: 
     case 6:
-        //something
-        break;
-        //something
     case 9:
-        // something
-        break;
     case 12:
-        //something
-        break;
-
+        
+for (int i; i<cow; ++i){
+    string move1, move2;
+    cout<< "Alg_1 move:";
+    cin>>move1;
+    b.placePiece(move1, ALG_1);
+    output<< "Alg_100-"<<move1<<endl;
+    cout<<"Alg_2 move:";
+    cin>>move2;
+    b.placePiece(move2, ALG_2);
+    output<< "Alg_200-"<<move2<<endl;
+}
+output<<"its a draw"<<endl<<endl;
+break;
     }
+}
 
 
-    g.output.close();
-    g.input.close();
+    output.close();
+    input.close();
     return 0;
 }
