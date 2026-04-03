c7efb3694785490221e907a610e998ee6d49aa26
Tshiamo77 <138474997+Tshiamo77@users.noreply.github.com>
2026-04-03 18:39:30 +0200

    Update
    No global variables. I am sorry I used them wanted to check the code works. And I only learnt about enums couple of days ago.

--- morabaraba/main.cpp
+++ morabaraba/main.cpp
@@ -5,25 +5,23 @@
 
 using namespace std;
 
-const int EMPTY = 0; 
-const int ALG_1 =1;
-const int ALG_2= 2;
+enum class state {EMPTY, ALG_1, ALG_2,};
 
 class Intersection{
 public:
     string label;
-    int status;
+    state status;
     vector<string> adjacencies;
 
-    Intersection(): status(EMPTY) {}
+    Intersection(): status(state::EMPTY) {}
     Intersection(string lbl) :
-    label(lbl), status (EMPTY) {}
+    label(lbl), status (state::EMPTY) {}
 };
 
 class Square{
     public:
         string name;
-        vector<Intersection> intersections;
+        vector<Intersection>intersections;
 
     Square (string n) : name(n) {
         for (int j = 0; j<8; ++j){
@@ -44,11 +42,11 @@ public:
         squares.push_back(Square("S1"));
         squares.push_back(Square("S2"));
     }
-        void placePiece(string pos, int player){
+        void placePiece(string pos, state player){
             int sIdx= pos[1]- '0';
             int iIdx= pos[3]- '0';
             if (sIdx>=0 && sIdx< 3 && iIdx>=0 && iIdx<8){
-                squares[sIdx].intersections[iIdx].status= player;
+                squares[sIdx].intersections[iIdx].status = player;
             }
         }
     };
@@ -80,19 +78,17 @@ for (int i; i<cow; ++i){
     string move1, move2;
     cout<< "Alg_1 move:";
     cin>>move1;
-    b.placePiece(move1, ALG_1);
+    b.placePiece(move1, state::ALG_1);
     output<< "Alg_100-"<<move1<<endl;
     cout<<"Alg_2 move:";
     cin>>move2;
-    b.placePiece(move2, ALG_2);
+    b.placePiece(move2, state::ALG_2);
     output<< "Alg_200-"<<move2<<endl;
 }
 output<<"its a draw"<<endl<<endl;
 break;
     }
 }
-
-
     output.close();
     input.close();
     return 0;
